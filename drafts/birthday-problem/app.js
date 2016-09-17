c552c88466c3f3b2b1020e822943b9c380c31691
Alex Kras <alex@Alexs-MacBook-Pro-2.local>
2016-09-17 15:09:01 -0700

    Re-sample logic

--- drafts/birthday-problem/app.js
+++ drafts/birthday-problem/app.js
@@ -1,30 +1,47 @@
-var birthdayData = [];
-fetch("birthdays-smaller.json")
-    .then(res => res.json())
-    .then(json => birthdayData = json)
-    .then(() => onDataLoad());
-function getRandomInt(max) {
-    if(!max) throw new Error("Max is required");
-    max = Math.floor(max);
-    return Math.floor(Math.random() * max);
-}
-function getDayAndMonth(birthday) {
-    return birthday.split(" ").splice(0, 2).join(" ")
-}
-function onDataLoad() {
-    var bdCount = birthdayData.length;
-    var numToSample = 15;
-    var selected = [];
-    for(var i = 0; i < numToSample; i++){
-        var bdId = getRandomInt(bdCount);
-        var bd = birthdayData[bdId];
-        selected.push(bd);
+(function () {
+    function getRandomInt(max) {
+        if(typeof max === "undefined" ) throw new Error("Max is required");
+        max = Math.floor(max);
+        return Math.floor(Math.random() * max);
     }
-    console.log(selected);
-    new Vue({
-        el: '#app',
-        data: {
-            birthdays: selected
+    function getDayAndMonth(birthday) {
+        return birthday.split(" ").splice(0, 2).join(" ")
+    }
+    function getRandomSample(numToSample){
+        var bdCount = birthdayData.length;
+        var randomActors = [];
+        for(var i = 0; i < numToSample; i++){
+            var bdId = getRandomInt(bdCount);
+            var bd = birthdayData[bdId];
+            randomActors.push(bd);
         }
-    });
-}
+        return randomActors;
+    }
+    var birthdayData = [];
+    var vm;
+    function setUpView(){
+        vm = new Vue({
+            el: '#app',
+            data: {
+                birthdays: getRandomSample(15),
+                sampleSize: null
+            },
+            beforeCompile: function(){
+                
+            },
+            methods: {
+                reSample: function(){
+                    var sampleSize = Number.parseInt(vm.sampleSize, 10);
+                    vm.$set('birthdays', getRandomSample(sampleSize)); 
+                }
+            }
+        });
+    }
+    setTimeout(function(){
+        fetch("birthdays-smaller.json")
+                    .then(res => res.json())
+                    .then(json => birthdayData = json)
+                    .then(setUpView)
+                    .then(() => vm.reSample());
+    }, 100)    
+})();
