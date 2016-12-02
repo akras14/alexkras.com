8382947f816dc27d1e084d6bbf7dc6040230b5f1
Alex Kras <akras14@gmail.com>
2016-12-02 09:48:30 -0800

    Moved birthday problem to posts folder

---
+++ posts/2016/birthday-problem/test.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python3
+%matplotlib inline
+import json, random
+
+# Load birthdays
+with open('birthdays-smaller.json') as data_file:
+  brithdayData = json.load(data_file)
+
+# Get day and month from the birthday string
+def getDayAndMonth(birthday):
+  return " ".join(birthday.split()[:2])
+
+birthIdRange = len(brithdayData)
+
+def oneRun(numToSample):
+  testedBirthdays = []
+  matchFound = False
+  for i in range(0, numToSample):
+    randomId = random.randint(0, birthIdRange - 1)
+    for tested in testedBirthdays:
+      randomBirthdayDate = getDayAndMonth(brithdayData[randomId]['db'])
+      testedDate = getDayAndMonth(tested['db'])
+      if randomBirthdayDate == testedDate:
+        matchFound = True
+        # print("Found a match")
+        # print(brithdayData[randomId])
+        # print("And")
+        # print(tested)
+        # print()
+    if(matchFound == True):
+      return matchFound
+    else:
+      testedBirthdays.append(brithdayData[randomId])
+  return matchFound
+
+# Kick off the test
+def runABatch():
+  matchesFound = 0
+  for i in range(0, 1000):
+    if oneRun(22) == True:
+      matchesFound += 1
+  return matchesFound*100/1000
+
+total = 0
+for i in range(0,10):
+  total += runABatch()
+  print(i)
+
+print("Percentage match " + str(total/10))
